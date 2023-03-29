2eaf483a2c27daf68e578af10606db11285e981c
Pietromuresu <muresupietro66@gmail.com>
2023-03-29 17:22:23 +0200

    add event listener let's play button

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,10 +1,20 @@
 
 
 const grid = document.getElementById('grid');
+const gridContainer = document.querySelector('.container-grid ')
+const generate = document.querySelector('.buttons-container');
+
+
+
+generate.addEventListener('click', function(){
+  generate.classList.add('hide')
+  gridContainer.classList.remove('hide')
+})
+
 
 
 for (let i = 0; i < 100; i++){
   const newDiv = document.createElement("div");
   grid.appendChild(newDiv);
-  newDiv.classList.add('square')
+  newDiv.classList.add('square');
 }
