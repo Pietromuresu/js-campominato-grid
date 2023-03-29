3131c1decee9fbc4cc1137db384c5158023fe7cc
Pietromuresu <muresupietro66@gmail.com>
2023-03-29 17:30:32 +0200

    newDiv toggle change background color

--- assets/scripts.js
+++ assets/scripts.js
@@ -8,13 +8,21 @@ const generate = document.querySelector('.buttons-container');
 
 generate.addEventListener('click', function(){
   generate.classList.add('hide')
-  gridContainer.classList.remove('hide')
+  gridContainer.classList.remove('hide');
+  
+  
+  for (let i = 0; i < 100; i++){
+  const newDiv = document.createElement("div");
+  grid.appendChild(newDiv);
+  newDiv.classList.add('square');
+
+  newDiv.addEventListener('click', function(){
+    newDiv.classList.toggle('clicked')
+  })
+}
+
 })
 
 
 
-for (let i = 0; i < 100; i++){
-  const newDiv = document.createElement("div");
-  grid.appendChild(newDiv);
-  newDiv.classList.add('square');
-}
+
